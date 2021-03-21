23e5fdef3d09693ce882711954229f76884acacd
Julián Bajo <julyandillo@gmail.com>
2021-03-21 18:33:29 +0100

    Añandiendo un sidebar desplegable a la derecha

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ import ModalNueva from "./modalNueva.js";
 document.addEventListener('DOMContentLoaded', () => {
 	const modalGenerar = new ModalGenerar('modal');
 	const modalNuevo = new ModalNueva();
+	const sidebar = document.querySelector('.sidebar-btn').onclick = () => showSidebar();
 });
 
+function showSidebar() {
+	const div = document.querySelector('.page');
+	const sidebar = document.getElementById('sidebar');
+	if (!div.classList.contains('page-sidebar-on')) {
+		div.classList.add('page-sidebar-on');
+		sidebar.classList.remove('d-none');
+	} else {
+		div.classList.remove('page-sidebar-on');
+		sidebar.classList.add('d-none');
+	}
+}
